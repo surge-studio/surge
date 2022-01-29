c4b31bd94ea5dc9aece248b34c1869644b4f7c08
Hayden Barnett <hello@haydenbarnett.com>
2022-01-29 19:35:40 +1100

    Improve next, ts, postcss and eslint config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const { createSecureHeaders } = require('next-secure-headers');
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async headers() {
+    return [{ source: '/(.*)', headers: createSecureHeaders() }];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
