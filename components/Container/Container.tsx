82be43e229ce6e087c940228bc1aeb152a2b45de
Hayden Barnett <hello@haydenbarnett.com>
2023-01-06 12:14:00 +1100

    Fix container props

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -1,8 +1,10 @@
-import type { FC } from 'react';
+import type { FC, ReactNode } from 'react';
 
 import styles from './styles.module.css';
 
-type ContainerProps = {};
+type ContainerProps = {
+  children?: ReactNode;
+};
 
 export const Container: FC<ContainerProps> = ({ children }) => {
   return <div className={styles.container}>{children}</div>;
